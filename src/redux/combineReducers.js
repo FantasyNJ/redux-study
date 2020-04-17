966ef43ad5251125c76135d2fd4563bbc60e9ab5
宁杰 <ningjie@qijiayoudao.com>
2020-04-17 14:24:01 +0800

    add commet

--- src/redux/combineReducers.js
+++ src/redux/combineReducers.js
@@ -2,9 +2,12 @@ export default function combineReducers(reducers) {
   return function combination(state={}, action) {
     let nextState = {};
     let hasChanged = false; //状态是否改变
+    // 把所有reducers都遍历一遍，所以redux在这边存在性能问题
     for(let key in reducers) {
+      // 单个reducer返回的state
       const previousStateForKey = state[key];
       const nextStateForKey = reducers[key](previousStateForKey, action);
+      // 所有reducers返回的state合集
       nextState[key] = nextStateForKey;
       //只有所有的 nextStateForKey 均与 previousStateForKey 相等时，hasChanged 的值 false
       hasChanged = hasChanged || nextStateForKey !== previousStateForKey;
